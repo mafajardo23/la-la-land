e72d9c273c5ba6fadc7ad003e0353285956a1e92
mafajardo23 <mafajardo@davidson.edu>
2026-02-25 10:38:59 -0500

    Piano works and text

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 //QWERTY APP ***************************************************
-const synth = new Tone.Synth().toDestination();
+
+// Use a piano sampler instead of a synth
+const piano = new Tone.Sampler({
+    urls: {
+        C4: "C4.mp3",
+        "D#4": "Ds4.mp3",
+        "F#4": "Fs4.mp3",
+        A4: "A4.mp3"
+    },
+    release: 3,
+    baseUrl: "https://tonejs.github.io/audio/salamander/"
+}).toDestination();
 
 const keyboard = new AudioKeys();
 
+
 keyboard.down((key) => {
     console.log(key);
-    synth.triggerAttackRelease(key.frequency, "8n");
+    if (key.frequency) {
+        piano.triggerAttackRelease(key.frequency, "8n");
+    }
 });
